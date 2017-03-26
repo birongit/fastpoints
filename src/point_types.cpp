68f2b67b9967b935e2ebdfd452336d1c2f43846b
birongit <birgithenke90@gmail.com>
2017-03-26 22:19:18 +0200

    New point type added

--- src/point_types.cpp
+++ src/point_types.cpp
@@ -1,5 +1,9 @@
 #include "point_types.h"
 
+PointXYZI::PointXYZI(double x, double y, double z, double i) :
+        x(x), y(y), z(z), i(i)
+{ }
+
 std::ostream& operator<< (std::ostream &out, const PointXYZI &point)
 {
 
@@ -21,4 +25,41 @@ std::string PointXYZI::TypeString() {
 
 std::string PointXYZI::FieldsString() {
  return std::string("x y z i");
+}
+
+PointXYZINormal::PointXYZINormal(PointXYZI point) :
+        PointXYZI(point)
+{ }
+
+PointXYZINormal::PointXYZINormal(PointXYZI point, double n_x, double n_y, double n_z) :
+        PointXYZI(point), n_x(n_x), n_y(n_y), n_z(n_z)
+{ }
+
+PointXYZINormal::PointXYZINormal(double x, double y, double z, double i, double n_x, double n_y, double n_z) :
+        PointXYZI(x,y,z,i), n_x(n_x), n_y(n_y), n_z(n_z)
+{ }
+
+std::string PointXYZINormal::SizeString() {
+    return std::string("4 4 4 4 4 4 4");
+}
+
+std::string PointXYZINormal::TypeString() {
+    return std::string("F F F F F F F");
+}
+
+std::string PointXYZINormal::FieldsString() {
+    return std::string("x y z i normal_x normal_y normal_z");
+}
+
+std::ostream &operator<<(std::ostream &out, const PointXYZINormal &point) {
+
+    out << (float) point.x   << " "
+    << (float) point.y   << " "
+    << (float) point.z   << " "
+    << (float) point.i   << " "
+    << (float) point.n_x << " "
+    << (float) point.n_y << " "
+    << (float) point.n_z;
+
+    return out;
 }
--- src/point_types.h
+++ src/point_types.h
@@ -7,8 +7,8 @@
 class PointXYZI {
 public:
     PointXYZI() { };
-    PointXYZI(double x, double y, double z, double i): x(x), y(y), z(z), i(i) { };
-    ~PointXYZI() { };
+    PointXYZI(double x, double y, double z, double i);
+    virtual ~PointXYZI() { };
 
     static std::string SizeString();
     static std::string TypeString();
@@ -17,6 +17,23 @@ public:
     friend std::ostream& operator<< (std::ostream &out, const PointXYZI &point);
 
     double x,y,z,i;
+};
+
+class PointXYZINormal : public PointXYZI {
+public:
+    PointXYZINormal() { };
+    PointXYZINormal(PointXYZI point);
+    PointXYZINormal(PointXYZI point, double n_x, double n_y, double n_z);
+    PointXYZINormal(double x, double y, double z, double i, double n_x, double n_y, double n_z);
+    ~PointXYZINormal() { };
+
+    static std::string SizeString();
+    static std::string TypeString();
+    static std::string FieldsString();
+
+    friend std::ostream& operator<< (std::ostream &out, const PointXYZINormal &point);
+
+    double n_x, n_y, n_z;
 };
 
 
